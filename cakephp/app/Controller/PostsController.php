a7b9b17bf3da03929bc49008d5602d7247cbb97a
Anna Yudina <anna.yudina16@gmail.com>
2014-12-03 15:11:46 -0700

    This is with the delete function

--- cakephp/app/Controller/PostsController.php
+++ cakephp/app/Controller/PostsController.php
@@ -5,5 +5,18 @@ class PostsController extends AppController {
 	
 	public function index(){
 		$this->set ('posts', $this->Post->find('all'));
+		// this is the delete function 
+	public function delete($id) {
+    if ($this->request->is('get')) {
+        throw new MethodNotAllowedException();
+    }
+
+    if ($this->Post->delete($id)) {
+        $this->Session->setFlash(
+            __('The post with id: %s has been deleted.', h($id))
+        );
+        return $this->redirect(array('action' => 'index'));
+    }
+}
 	}
 }
