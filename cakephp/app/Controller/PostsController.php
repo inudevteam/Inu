ba138ed5441fcc4e228381d6734c69810983e108
Anna Yudina <anna.yudina16@gmail.com>
2014-12-03 15:20:26 -0700

    Update PostsController.php
    I have added the edit function in php

--- cakephp/app/Controller/PostsController.php
+++ cakephp/app/Controller/PostsController.php
@@ -5,6 +5,30 @@ class PostsController extends AppController {
 	
 	public function index(){
 		$this->set ('posts', $this->Post->find('all'));
+		// this is the edit function 
+	public function edit($id = null) {
+    if (!$id) {
+        throw new NotFoundException(__('Invalid post'));
+    }
+
+    $post = $this->Post->findById($id);
+    if (!$post) {
+        throw new NotFoundException(__('Invalid post'));
+    }
+
+    if ($this->request->is(array('post', 'put'))) {
+        $this->Post->id = $id;
+        if ($this->Post->save($this->request->data)) {
+            $this->Session->setFlash(__('Your post has been updated.'));
+            return $this->redirect(array('action' => 'index'));
+        }
+        $this->Session->setFlash(__('Unable to update your post.'));
+    }
+
+    if (!$this->request->data) {
+        $this->request->data = $post;
+    }
+}
 		// this is the delete function 
 	public function delete($id) {
     if ($this->request->is('get')) {
