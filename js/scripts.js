70a919d34e3591c658eff2c4b807c00c7e51efb2
matteisc <matteisc@colorado.edu>
2014-12-03 17:25:51 -0700

    customized current location icon

--- js/scripts.js
+++ js/scripts.js
@@ -65,15 +65,17 @@ function initialize() {
   if(navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
       var pos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+        var image = '../img/myloc.png';
       var marker = new google.maps.Marker({
         position: pos,
         map: map,
-        title: 'Hello World!'
+        icon: image,
+        title: 'You are here!'
       })
-/*      google.maps.event.addListener(marker, 'click', function() {
+     google.maps.event.addListener(marker, 'click', function() {
         map.setZoom(18);
         map.setCenter(marker.getPosition());
-      });*/
+      });
     }, function() {
       handleNoGeolocation(true);
     });
@@ -89,13 +91,13 @@ function initialize() {
     } else {
       var content = 'Error: Your browser doesn\'t support geolocation.';
     }
-/*    var options = {
+ var options = {
       map: map,
       position: new google.maps.LatLng(40.006711,-105.263623),
       content: content
-    };*/
-/*    var infowindow = new google.maps.InfoWindow(options);
-    map.setCenter(options.position);*/
+    }
+ var infowindow = new google.maps.InfoWindow(options);
+    map.setCenter(options.position);
   }
   
   // FUNC001: display search bar and tie to database
