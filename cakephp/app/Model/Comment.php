3bcc0780ac32947a13916d3f609ed9a37bc11729
Anna Yudina <anna.yudina16@gmail.com>
2014-12-09 01:32:17 -0700

    Create Comment.php

---
+++ cakephp/app/Model/Comment.php
@@ -0,0 +1,39 @@
+<?php
+class Comment extends AppModel {
+	var $name = 'Comment';
+	var $displayField = 'name';
+	var $validate = array(
+		'name' => array(
+			'notempty' => array(
+				'rule' => array('notempty'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+		'content' => array(
+			'notempty' => array(
+				'rule' => array('notempty'),
+				//'message' => 'Your custom message here',
+				//'allowEmpty' => false,
+				//'required' => false,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+	);
+	//The Associations below have been created with all possible keys, those that are not needed can be removed
+
+	var $belongsTo = array(
+		'Post' => array(
+			'className' => 'Post',
+			'foreignKey' => 'post_id',
+			'conditions' => '',
+			'fields' => '',
+			'order' => ''
+		)
+	);
+}
+?>
